de58b0959290ceafe796c428a39899ea14814444
Kyle Andrews <kylea@page.works>
2019-08-05 07:33:45 -0400

    Added function to get all compiled JS file paths using Glob

--- bundler.js
+++ bundler.js
@@ -30,11 +30,60 @@ class Bundler
         try
         {
             console.log('Running the bundler');
+            const files = this.findFiles();
+            this.getImportStatements(files)// Parse files for import statements
+            // Remove duplicate import statements
+            // Get node modules
+            // Get local scripts
+            // Bundle node modules
+            // Bundle local scripts
+            // Update import statements
         }
         catch (error)
         {
             throw error;
         }
+    }
+
+    findFiles()
+    {
+        return new Promise((resolve, reject)=>{
+            glob('./_compiled/**/*.js', (error, files)=>{
+                if(error)
+                {
+                    reject(error);
+                }
+
+                resolve(files);
+            });
+        });
+    }
+
+    getImportStatements(files)
+    {
+        return new Promise((resolve, reject)=>{
+            if(!files)
+            {
+                reject('Can\'t find imports statements from non-existent files');
+            }
+
+            for (let i = 0; i < files.length; i++)
+            {
+                fs.readFile(files[i], (error, buffer)=>{
+                    if(error)
+                    {
+                        reject(error);
+                    }
+
+                    const data = buffer.toString();
+
+                    if(data.match(/(import).*(from)/g))
+                    {
+                        
+                    }
+                });
+            }
+        });
     }
 }
 
